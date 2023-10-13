5e053bcd1a286b6fd41c45cb0eb92df7c2dd16f0
randomqwerty <randomqwerty@users.noreply.github.com>
2023-10-12 20:26:13 -0400

    Update KR/TW to 3.03

--- Program.cs
+++ Program.cs
@@ -223,7 +223,7 @@ namespace GFDecompress
                     Downloader kr = new Downloader("kr");
                     kr.downloadStc();
                     kr.downloadAsset();
-                    clientVersion = "3020";
+                    clientVersion = "3030";
                     break;
                 case "en":
                     Console.WriteLine("\n====EN Data download====");
@@ -251,7 +251,7 @@ namespace GFDecompress
                     Downloader tw = new Downloader("tw");
                     tw.downloadStc();
                     tw.downloadAsset();
-                    clientVersion = "3020";
+                    clientVersion = "3030";
                     break;
             }
 
