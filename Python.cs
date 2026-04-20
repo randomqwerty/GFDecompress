a87750d648e105d32b70e724cb780baae2dc677f
randomqwerty <randomqwerty@users.noreply.github.com>
2026-04-19 21:29:40 -0400

    Fix Python hardcoding

--- Python.cs
+++ Python.cs
@@ -15,16 +15,8 @@ namespace GFDecompress
         public static void run(string cmd, string args, ref StreamReader output, ref StreamReader error  ) {
             ProcessStartInfo start = new ProcessStartInfo();
             string envVar =  Environment.GetEnvironmentVariable("LOCALAPPDATA");
-            string pyPath = $@"{envVar}\\Programs\\Python\\Python38\\python.exe";
-            
-            while (!File.Exists(pyPath))
-            {
-                Console.WriteLine("Python executable was not found, please enter the verssion number without periods below.");
-                Console.WriteLine("For example, v3.8 = 38, v3.9 = 39");
-                pyPath = $@"{envVar}\\Programs\\Python\\Python" + Console.ReadLine() +"\\python.exe";
-            }
 
-            start.FileName = pyPath;
+            start.FileName = "python";
             start.Arguments = string.Format("{0} {1}",cmd, args);
             start.UseShellExecute = false;
             start.CreateNoWindow = false;
